87af615aabd7039d6ff2e0f9f71c2007cbbb76ea
richemeflo <florian.richeme@gmail.com>
2025-08-11 19:58:00 +0200

    clean

--- backend/src/config/database.js
+++
@@ -1,9 +0,0 @@
-const { Pool } = require('pg');
-
-// Railway fournit DATABASE_URL automatiquement
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
-});
-
-module.exports = pool;
