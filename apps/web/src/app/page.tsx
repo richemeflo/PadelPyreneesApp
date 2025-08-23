1e4a0821db82b6739b5050150b5d403fa27b9deb
richemeflo <florian.richeme@gmail.com>
2025-08-23 17:11:06 +0200

    FRE - fix import + start homepage

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { Plus } from "lucide-react@0.487.0";
+import { Plus } from "lucide-react";
 
 import { Card, CardContent } from "../components/ui/card";
 import { Button } from "../components/ui/button";
